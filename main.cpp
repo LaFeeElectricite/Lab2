711fd51dcd4b30be89f8ae763749b9120704bda6
jongwoochae <albertchae@snu.ac.kr>
2024-06-07 17:01:15 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -9,9 +9,12 @@ vector<int> vec;
 void func(int cnt) {
    if(cnt == M) {
 	for(auto elem : vec) cout << elem << ' ';
+	cout << endl;
 	return;
    }
-   int b = vec.back();
+   int b;
+   if(cnt) b = vec.back();
+   else b = 0;
    if(b == N) return;
    for(int i=b+1; i<=N; i++) {
 	vec.push_back(i);
