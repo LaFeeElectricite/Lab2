17dddbdcb233dc0e0a8f33184fc9442a3defbede
jongwoochae <albertchae@snu.ac.kr>
2024-06-07 16:44:28 +0900

    modify func

--- main.cpp
+++ main.cpp
@@ -7,13 +7,17 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
-
-
-
-
-
-
+   if(cnt == M) {
+	for(auto elem : vec) cout << elem << ' ';
+	return;
+   }
+   int b = vec.back();
+   if(b == N) return;
+   for(int i=b+1; i<=N; i++) {
+	vec.push_back(i);
+	func(cnt+1);
+	vec.pop_back();
+   }
 }
 
 int main() {
